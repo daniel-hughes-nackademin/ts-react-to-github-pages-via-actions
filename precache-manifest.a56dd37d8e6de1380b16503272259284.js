878e81defa780a6617fc110f34a92a06609c3fab
daniel-hughes-nackademin <daniel-hughes-nackademin@users.noreply.github.com>
2020-09-09 11:46:08 +0000

    deploy: 839ea9466e3a65542bf7f731f1b639a5cbc98214

---
+++ precache-manifest.a56dd37d8e6de1380b16503272259284.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd896962fe8c8a47d1e778bc4dddcae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7abeeef5a18a69d4890",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a4ed6332d8bad011279",
+    "url": "/static/js/2.bec82807.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bec82807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7abeeef5a18a69d4890",
+    "url": "/static/js/main.bc09e548.chunk.js"
+  },
+  {
+    "revision": "1bbe701211838f9b1394",
+    "url": "/static/js/runtime-main.a3903d4a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
